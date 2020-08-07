f5f4eb110d9ca3b31accfda46db3a7b4bccd3869
Alice1017 <genocidedragon@gmail.com>
2020-08-07 15:50:45 +0900

    TEST: validate.ts

---
+++ test/validate.test.ts
@@ -0,0 +1,33 @@
+// Type
+// Targets
+import { validateFlags } from "../src/validate";
+import { errors } from "../src/errors";
+// Libraries
+import { fancy } from "fancy-test";
+import assert = require("assert");
+
+
+describe("src/validate.ts", () => {
+
+  fancy
+    .do(() => {
+      validateFlags({repository: "peco"});
+    })
+    .catch(error => {
+      assert.equal(
+        error.message,
+        errors.got.invalid.repository
+      );
+    })
+    .it("Raise error when got invalid repository.");
+
+  it("Return flags when through validation", () => {
+    const validFlags = {repository: "peco/peco"};
+    const flags = validateFlags(validFlags);
+    assert.deepEqual(
+      flags,
+      validFlags
+    );
+  });
+
+});
